f7e1b7fc1ede796961d513e4ad70dc723c65eb69
Mohsin Javaid <iammohssin@users.noreply.github.com>
2021-09-19 14:19:01 +0500

    Update

---
+++ index.js
@@ -0,0 +1,11 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+    if ( h == 0){
+        h = 12;
+    }
+}
+showTime();
