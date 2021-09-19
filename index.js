34d1a9eaf7c11a377b3586e9556d42a6f3064c53
Mohsin Javaid <iammohssin@users.noreply.github.com>
2021-09-19 14:52:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ function showTime(){
     if ( h == 0){
         h = 12;
     }
+    if (h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+    setTimeout(showTime, 1000);
 }
 showTime();
